b42da8e231549c989d7c945a263a8760beee5853
juan <juan@Juans-MacBook-Pro.local>
2019-06-21 15:53:51 -0300

    fix field in import

--- ss.sh
+++ ss.sh
@@ -1,8 +1,8 @@
-shortstack_path=/Users/juan/Documents/manu/dev/vms/bio/mirna_mite/scripts/ShortStack
-data_path=/Volumes/toshiba/temp/data/sRNASeq
-genome_path=/Volumes/toshiba/temp/data/wheat/Triticum_aestivum.IWGSC.dna.toplevel.fa
-res_path=/Users/juan/Documents/manu/dev/vms/bio/mirna_mite/data
-locifile_path=/Volumes/toshiba/temp/data/mitesall.locifile.csv
+shortstack_path=/home/crescentejuan/Documents/mirna_mite/ShortStack
+data_path=/home/crescentejuan/Documents/data/sRNASeq
+genome_path=/mnt/disk/iwgsc_bowtie/Triticum_aestivum.IWGSC.dna.toplevel.fa
+res_path=/home/crescentejuan/Documents/mirna_mite/data/res
+locifile_path=/home/crescentejuan/Documents/data/mitesall.locifile.csv
 #all sRNA and loci
 cd $shortstack_path
 
@@ -13,13 +13,14 @@ cd $shortstack_path
     $data_path/21dpiR3T.trimmed.fq.gz \
     --genomefile $genome_path \
     --outdir $res_path/1_all \
-    --sort_mem 10G \
+    --sort_mem 9G \
+#    --bowtie_cores 2 \
 --bowtie_m all --foldsize 1000
 
 ./ShortStack \
-    --bamfile $res_path/all/merged_alignments.bam \
+    --bamfile $res_path/1_all/merged_alignments.bam \
     --genomefile $genome_path \
     --bowtie_m all --foldsize 1000 \
     --locifile $locifile_path \
-    --sort_mem 3500M \
+    --sort_mem 9G \
     --outdir --outdir $res_path/1_mites \
