6ab777dfafd07eeb7a19e33c0c01e3c36c656728
Juan Manuel Crescente <you@example.com>
2019-06-28 15:23:54 -0300

    update sw

--- 1_data_ss/6_sun_cleave.sh
+++ 1_data_ss/6_sun_cleave.sh
@@ -1,1 +1,12 @@
-./scripts/CleaveLand4/CleaveLand4.pl -e $fasta_degradome -u $miRNAs -n $transcriptome -t -o $plots > $out_deg1
+path_cleaveland=/home/juan/Desktop/juan/bio/mirna_mite/sw/CleaveLand4
+path_deg=/home/juan/Desktop/juan/bio/mirna_mite/data/sun/degradome
+path_gsta=/home/juan/Desktop/juan/bio/mirna_mite/sw/CleaveLand4/GSTAr_v1-0
+miRNAs=
+transcriptome=
+plots=
+out_deg=
+export PATH=$PATH:$path_gsta
+for file_name in SRR1197125  SRR1197127 SRR1197126  SRR1197128
+do
+    ./${path_cleaveland}/CleaveLand4.pl -e $path_deg/$file_name.trimmed.fastq.gz -u $miRNAs -n $transcriptome -t -o $plots > $out_deg
+done
