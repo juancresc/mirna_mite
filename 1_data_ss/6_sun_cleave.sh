0f9c742839034b9fdda958d26c4344705bc5af4a
Juan Manuel Crescente <you@example.com>
2019-07-02 14:51:55 -0300

    update sw

--- 1_data_ss/6_sun_cleave.sh
+++ 1_data_ss/6_sun_cleave.sh
@@ -1,12 +1,14 @@
+set -x 
 path_cleaveland=/home/juan/Desktop/juan/bio/mirna_mite/sw/CleaveLand4
 path_deg=/home/juan/Desktop/juan/bio/mirna_mite/data/sun/degradome
 path_gsta=/home/juan/Desktop/juan/bio/mirna_mite/sw/CleaveLand4/GSTAr_v1-0
-miRNAs=
-transcriptome=
-plots=
-out_deg=
+path_mirna_fasta=/home/juan/Desktop/juan/bio/mirna_mite/data/res/mirnas.y.fasta
+transcriptome=/home/juan/Desktop/juan/bio/data/IWGSC/42/Triticum_aestivum.IWGSC.cdna.all.fa
+plots=/home/juan/Desktop/juan/bio/mirna_mite/data/res/sun_plots
+out_deg=/home/juan/Desktop/juan/bio/mirna_mite/data/res/sun_deg
 export PATH=$PATH:$path_gsta
 for file_name in SRR1197125  SRR1197127 SRR1197126  SRR1197128
 do
-    ./${path_cleaveland}/CleaveLand4.pl -e $path_deg/$file_name.trimmed.fastq.gz -u $miRNAs -n $transcriptome -t -o $plots > $out_deg
+    #gunzip $path_deg/$file_name.trimmed.fastq.gz > $path_deg/$file_name.trimmed.fastq
+    ${path_cleaveland}/CleaveLand4.pl -e $path_deg/$file_name.trimmed.fastq -u $path_mirna_fasta -n $transcriptome -t -o $plots > $out_deg
 done
