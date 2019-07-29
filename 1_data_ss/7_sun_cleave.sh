405b01634a19f8cfedcaa8a3b43663a802bfb5b5
Juan Manuel Crescente <you@example.com>
2019-07-29 15:31:45 -0300

    layour images

--- 1_data_ss/7_sun_cleave.sh
+++ 1_data_ss/7_sun_cleave.sh
@@ -5,11 +5,11 @@ set -x
 path_cleaveland=/home/juan/Desktop/juan/bio/mirna_mite/sw/CleaveLand4
 path_deg=/home/juan/Desktop/juan/bio/mirna_mite/data/sun/degradome
 path_mirna_fasta=/home/juan/Desktop/juan/bio/mirna_mite/data/res/mirna.y.fasta
-path_mirna_fasta=/home/juan/Desktop/juan/bio/mirna_mite/data/res/mirna.y.trep.fasta
+#path_mirna_fasta=/home/juan/Desktop/juan/bio/mirna_mite/data/res/mirna.y.trep.fasta
 transcriptome=/home/juan/Desktop/juan/bio/mirna_mite/data/Triticum_aestivum.IWGSC.cdna.all.fa
 transcriptome=/home/juan/Desktop/juan/bio/mirna_mite/data/fixed.cdna.all.fa
 plots=/home/juan/Desktop/juan/bio/mirna_mite/data/res/sun_plots
-out_deg=/home/juan/Desktop/juan/bio/mirna_mite/data/res/sun_deg_
+out_deg=/home/juan/Desktop/juan/bio/mirna_mite/data/res/sun_deg
 
 path_gsta=/home/juan/Desktop/juan/bio/mirna_mite/sw/CleaveLand4/GSTAr_v1-0
 export PATH=$PATH:$path_gsta
@@ -19,15 +19,15 @@ path_samtools=/home/juan/Desktop/juan/bio/sw/samtools-0.1.19
 path_samtools=/home/juan/Desktop/juan/bio/sw/samtools-1.2
 export PATH=$PATH:$path_samtools
 
-file_name=SRR1197127
-mkdir $plots/${file_name}
-${path_cleaveland}/CleaveLand4.pl -e $path_deg/${file_name}.trimmed.21.fasta -u $path_mirna_fasta -n $transcriptome -t -o $plots/${file_name} > ${out_deg}_${file_name} 2> ${out_deg}_${file_name}.log
+file_name=SRR1197125
+#mkdir $plots/${file_name
+${path_cleaveland}/CleaveLand4.pl -e $path_deg/${file_name}.trimmed.21.fasta -u $path_mirna_fasta -n $transcriptome -t -o $plots/${file_name} > ${out_deg}/${file_name} 2> ${out_deg}/${file_name}.log
 
 #for file_name in SRR1197125  SRR1197126 SRR1197127  SRR1197128
-for file_name in SRR1197126 SRR1197127  SRR1197128
-do
-    mkdir $plots/${file_name}
-    ${path_cleaveland}/CleaveLand4.pl -e $path_deg/${file_name}.trimmed.21.fasta -u $path_mirna_fasta -n $transcriptome -t -o $plots/${file_name} > $out_deg${file_name}
-done
+#for file_name in SRR1197125 SRR1197126 SRR1197127 SRR1197128
+#do
+#    mkdir $plots/${file_name}
+#    ${path_cleaveland}/CleaveLand4.pl -e $path_deg/${file_name}.trimmed.21.fasta -u $path_mirna_fasta -n $transcriptome -t -o $plots/${file_name} > ${out_deg}_${file_name} 2> ${out_deg}_${file_name}.log
+#done
 
 #${path_cleaveland}/CleaveLand4.pl -e $path_deg/SRR1197125.trimmed.fastq -u $path_mirna_fasta -n $transcriptome -t -o $plots > $out_deg
--- README.md
+++ README.md
@@ -1,15 +1,6 @@
-# Cleaveland error
-Finished with Query: mrcv_all_Cluster_34329 7B:223173899-223174050
+https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0019212#
 
-Found a total of 649 non-redundant putative slicing sites with p-values <= 1 and categories <= 4. Outputting results.
-sh: 1: chr5D: not found
-sh: 1: 282007012: not found
-sh: 1: 282007117: not found
-sh: 1: AGT: not found
-sh: 1: 113: not found
-sh: 1: F4436: not found
-sh: 1: chr5D: not found
-sh: 1: 282007012: not found
-sh: 1: 282007117: not found
-sh: 1: AGT: not found
-sh: 1: 113: not found
+https://www.annualreviews.org/doi/10.1146/annurev.arplant.57.032905.105218
+
+https://www.ncbi.nlm.nih.gov/pubmed/19037014
+https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2327354/
