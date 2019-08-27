075cf48ebd1f609fd79ab3d76cd2ab08fa59686f
Juan Manuel Crescente <you@example.com>
2019-08-27 14:27:44 -0300

    asd

---
+++ 2_analysis/test_examples.sh
@@ -0,0 +1,11 @@
+source /home/juan/Desktop/juan/bio/venv/bin/activate
+
+
+
+python /home/juan/Desktop/juan/bio/biopyutils/FAslicer.py -i 1A -s 492271825 -e 492271935 -f /home/juan/Desktop/juan/bio/data/IWGSC/42/Triticum_aestivum.IWGSC.dna.toplevel.fa
+
+>mrcv_all_Cluster_1205_1A_492271825_492271935 1A_492271825_492271935 
+CGCCCTGTGCCTGCTCTTCCTCGTCGCCGTCTCCTCCCTCCCCCTGCTCGCCCGCGCCGAGTGCGAGTGCGAGGCCGGCGGCGAGGAGGAGGAGCAGGACAAGGCGGGCT
+
+
+
